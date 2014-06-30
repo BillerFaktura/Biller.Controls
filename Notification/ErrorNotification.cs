972d9e67daecc4587674bb0b204a104e5b5ce1cd
Igor Lückel <igor.lueckel@googlemail.com>
2014-06-30 21:46:35 +0200

    - Added [ErrorNotification] - Removed "Duration" from [Notification]

---
+++ Notification/ErrorNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biller.Controls.Notification
+{
+    public class ErrorNotification : Notification
+    {
+        public ErrorNotification()
+            : base()
+        {
+        }
+    }
+}
