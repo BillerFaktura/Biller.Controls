038d29effdd2bf3997b0df447af0c6f69cd49a3a
Igor Lückel <igor.lueckel@googlemail.com>
2014-07-17 10:14:05 +0200

    Added UpdateNotification & DelegateCommand

---
+++ Notification/UpdateNotification.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Biller.Controls.Notification
+{
+    public class UpdateNotification : Notification
+    {
+        public UpdateNotification(Action<object> ChangelogClick, Action<object> UpdateNowClick, Action<object> UpdateLaterClick)
+            : base()
+        {
+            ChangelogCommand = new Biller.Controls.DelegateCommand(ChangelogClick);
+            UpdateNowCommand = new Biller.Controls.DelegateCommand(UpdateNowClick);
+            UpdateLaterCommand = new Biller.Controls.DelegateCommand(UpdateLaterClick);
+        }
+
+        private double progress;
+        public double Progress
+        {
+            get { return progress; }
+
+            set
+            {
+                if (progress == value) return;
+                progress = value;
+                OnPropertyChanged("Progress");
+            }
+        }
+
+        private System.Windows.Visibility progressBarVisibility = System.Windows.Visibility.Collapsed;
+        public System.Windows.Visibility ProgressBarVisibility
+        {
+            get { return progressBarVisibility; }
+
+            set
+            {
+                if (progressBarVisibility == value) return;
+                progressBarVisibility = value;
+                OnPropertyChanged("ProgressBarVisibility");
+            }
+        }
+
+        private System.Windows.Visibility textVisibility = System.Windows.Visibility.Visible;
+        public System.Windows.Visibility TextVisibility
+        {
+            get { return textVisibility; }
+
+            set
+            {
+                if (textVisibility == value) return;
+                textVisibility = value;
+                OnPropertyChanged("TextVisibility");
+            }
+        }
+
+        public ICommand ChangelogCommand { get; set; }
+        public ICommand UpdateNowCommand { get; set; }
+        public ICommand UpdateLaterCommand { get; set; }
+    }
+}
